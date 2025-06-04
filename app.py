69ef7d33ff433fe280c3beb7dbdee4df67fc3de6
Ivan_Diaz87 <ivandiaztpv@gmail.com>
2025-06-04 11:48:42 -0600

    Actualicé la indentation del código

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import plotly_express as px
 
 st.header('Información estadística de carros')
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-     hist_button = st.button('Construir histograma') # crear un botón
+hist_button = st.button('Construir histograma') # crear un botón
      
-     if hist_button: # al hacer clic en el botón
+if hist_button: # al hacer clic en el botón
          # escribir un mensaje
-         st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
          
          # crear un histograma
-         fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer")
      
          # mostrar un gráfico Plotly interactivo
-         st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
